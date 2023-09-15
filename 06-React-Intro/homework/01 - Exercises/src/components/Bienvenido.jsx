031f7718c6094094e354ce7065fc348617ba3ebb
Gilberto Díaz <diazp.gilberto@gmail.com>
2023-09-14 22:06:52 -0500

    first react exercise done

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const studentName = "";
+const studentName = "Gilberto";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+
+  return (
+    <div>
+      <h1>Mi primera app con React</h1>
+      <h3>{studentName}</h3>
+      <ul>{techSkills.map((skills) =><li>{skills}</li>)}</ul>
+    </div>
+  );
+
 }
 
 // Esto lo exportamos para los tests
