54e681255e46cbe4e377751d2e02a80efb18a318
Gilberto Díaz <diazp.gilberto@gmail.com>
2023-10-03 12:16:21 -0500

    fixed bug related to elements id

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import {Botones} from "./Botones.jsx";
+import { Botones } from "./Botones.jsx";
 
 const studentName = "Gilberto";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
-const alerts = { m1: "Aprobado", m2: "En curso" };
+const alerts = { m1: "Aprobado", m2: "Conseguiras tu BOOM en enero de 2024" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-
+  
+  
   return (
     <div>
-      <h1>Mi primera app con React</h1>
+      <h1>Hola soy Glberto Díaz</h1>
       <h3>{studentName}</h3>
-      <ul>{techSkills.map((skills) =><li>{skills}</li>)}</ul>
-     <Botones />
+      <ul>{techSkills.map((element,index) => <li key={index}>{element}</li>)}</ul>
+      <Botones alerts = {alerts}></Botones>
     </div>
   );
 
--- 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-export function Botones(alerts) {
+export function Botones({alerts}) {
 
     return (<div>
-        <button onClick={() => alert("Eres feliz!")}>Modulo 1</button>
-        <button onClick={() => alert("Gracias Dios")}>Modulo 2</button>
+        <button onClick={()=>alert(alerts.m1)}>Modulo 1</button>
+        <button onClick={()=>alert(alerts.m2)}>Modulo 2</button>
     </div>);
 }
 
