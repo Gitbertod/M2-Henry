ac0eeffcbf97901c347a172ea6676dd7ce524f2b
Gilberto Díaz <diazp.gilberto@gmail.com>
2023-10-16 15:10:47 -0500

    beginnig form Homework

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.modules.css'
 
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-export default function Contact () {
-  return <div>Crear Formulario</div>
+export function validate(inputs) {
+  let errors = {};
+
+  if (inputs.name === "") {
+    errors.name = "Se requiere un nombre"
+  }
+  if (inputs.message === "") {
+    errors.message = "Se requiere un mensaje"
+  }
+
+  if (!regexEmail.test(inputs.email)) {
+    errors.email = 'Debe ser un correo electrónico';
+  }
+  return errors;
+}
+
+export default function Contact() {
+  const [inputs, setInputs] = React.useState({
+    name: '',
+    email: '',
+    message: ''
+
+  })
+
+  const [errors, setErros] = React.useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = (event) => {
+    setInputs({
+      ...inputs,
+      [event.target.name]: event.target.value
+    })
+
+    setErros(validate({
+      ...inputs,
+      [event.target.name] : event.target.value,
+    }))
+  }
+
+  return <div>
+    <form>
+      <label>Nombre:</label>
+      <input name='name' placeholder='Escribe tu nombre...' type='text' value={inputs.name} onChange={handleChange}></input>
+
+      <label>Correo Electrónico:</label>
+      <input name='email' placeholder='Escribe tu email...' type='text' value={inputs.email} onChange={handleChange} ></input>
+
+      <label>Mensaje:</label>
+      <textarea name='message' placeholder='Escribe tu mensaje...' type="text" value={inputs.message} onChange={handleChange} ></textarea>
+      <button type='submit'>Enviar</button>
+    </form>
+
+  </div>
 }
